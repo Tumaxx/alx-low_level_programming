113b5d5f163954fc8b69276e5b80b7e4886a1921
Tumaxx <micsegun005@gmail.com>
2023-02-17 23:21:17 -0800

    5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -5,11 +5,11 @@
  * starting from 0
  * Retrun: Always 0
  */
-int main (void)
+int main(void)
 {
 	int num;
 
-	for(num = 0; num < 10; num++)
+	for (num = 0; num < 10; num++)
 		printf("%d", num);
 	printf("\n");
 	return (0);
