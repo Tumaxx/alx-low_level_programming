9d330bceef66efc2c422e0743967799c235a7021
Tumaxx <micsegun005@gmail.com>
2023-02-22 05:14:57 -0800

    Task 0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * main - teste function that prints if integer is positive or negative
+ * main - test function for positive or negative
  * Return: 0
  */
 int main(void)
@@ -10,5 +10,6 @@ int main(void)
 
 	i = 0;
 	positive_or_negative(i);
+
 	return (0);
 }
