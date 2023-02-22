5db94f02b23930f857a29a6d93246f55744129ff
Tumaxx <micsegun005@gmail.com>
2023-02-22 01:12:26 -0800

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -3,16 +3,16 @@
 /**
  * print_alphabet_x10 - prints alphabet 10 times
  */
-void print_alphabet_x10(void);
+void print_alphabet_x10(void)
 {
 	int ten;
-	char la;
+
+	char la
 
 	for (ten = 0; ten <= 9; ten++)
 	{
 		for (la = 'a'; la <= 'z'; la++)
 			_putchar(la);
-
-		_putchar('\n');
+	_putchar('\n');
 	}
 }
